6b0fbc1beee2892ee76115c2aecf4befe7bc81e8
Raul Franco <raul.franco@norvoz.es>
2022-09-16 13:42:14 +0200

    Diferentes rutas

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
-use axum::{
-    routing::get,
-    Router,
-};
-
 #[tokio::main]
 async fn main() {
-    // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    // run it with hyper on localhost:3000
+    use axum::{Router, routing::get};
+
+    // Rutas
+    let app = Router::new()
+        .route("/", get(get_raiz))
+        .route("/usuarios", get(get_usuarios))
+        .route("/contacto", get(get_contacto));
+
+    // Aplicacion corriendo en:  http://localhost:3000.
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn get_raiz() -> String {
+    String::from("raiz")
+}
+async fn get_usuarios()->String {
+    String::from("Ususarios")
+
+}
+async fn get_contacto()-> String {
+    String::from("Contacto")
 }
