d904bb50a690f775295959237054db12a0fdf834
Raul Franco <raul.franco@norvoz.es>
2022-09-16 16:34:42 +0200

    rutas con parametros

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,9 @@ async fn main() {
         .fallback(fallback.into_service())
         .route("/", get(get_raiz))
         .route("/usuarios", get(get_usuarios))
-        .route("/contacto", get(get_contacto));
+        .route("/usuario/:id", get(get_usuario))
+        .route("/contacto", get(get_contacto))
+        .route("/estado", get(get_con_estado));
 
     // Aplicacion corriendo en:  http://localhost:3000.
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -17,13 +19,23 @@ async fn main() {
         .unwrap();
 }
 
-async fn get_raiz() -> String {
+pub async fn get_con_estado()->(axum::http::StatusCode,String){(
+    axum::http::StatusCode::OK,String::from("Todo correcto")
+)}
+
+pub async fn get_raiz() -> String {
     String::from("raiz")
 }
-async fn get_usuarios() -> String {
+
+pub async fn get_usuario(axum::extract::Path(id):axum::extract::Path<String>) -> String {
+    format!("Usuario con id {}",id)
+}
+
+pub async fn get_usuarios() -> String {
     String::from("Ususarios")
 }
-async fn get_contacto() -> String {
+
+pub async fn get_contacto() -> String {
     String::from("Contacto")
 }
 
